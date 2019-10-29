c42b2d0e619f6f5d120fa41b3ed7e94cf33835a5
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2019-10-29 08:54:24 +0500

    Add random, ascending and descending access pattern

--- benchmarks/TwoArray_bench.cpp
+++ benchmarks/TwoArray_bench.cpp
@@ -6,6 +6,29 @@
 #include <random>
 #include <vector>
 
+void generate_random_access(std::vector<size_t> &offsets) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  std::uniform_int_distribution<size_t> distribution(0, offsets.size() - 1);
+
+  std::transform(
+      std::begin(offsets), std::end(offsets), std::begin(offsets),
+      [&distribution, &gen](size_t value) { return distribution(gen); });
+}
+
+void generate_ascending_access(std::vector<size_t> &offsets) {
+  size_t offset = 0;
+  std::transform(std::begin(offsets), std::end(offsets), std::begin(offsets),
+                 [&offset](size_t value) { return offset++; });
+}
+
+void generate_descending_access(std::vector<size_t> &offsets) {
+  size_t offset = offsets.size() - 1;
+  std::transform(std::begin(offsets), std::end(offsets), std::begin(offsets),
+                 [&offset](size_t value) { return offset--; });
+}
+
 template <int32_t S> void TwoArray(benchmark::State &state) {
   using value_type = typename Codility::TwoArray<S>::value_type;
 
@@ -15,17 +38,12 @@ template <int32_t S> void TwoArray(benchmark::State &state) {
   std::vector<value_type> input(size, 0);
 
   // generate access pattern
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::size_t max_index = size - 1;
-  std::uniform_int_distribution<size_t> distribution(0, max_index);
-
   std::vector<size_t> offsets;
   offsets.resize(size);
 
-  std::transform(
-      std::begin(offsets), std::end(offsets), std::begin(offsets),
-      [&distribution, &gen](size_t value) { return distribution(gen); });
+  // generate_random_access(offsets);
+  // generate_ascending_access(offsets);
+  generate_descending_access(offsets);
 
   int32_t index = 0;
   for (auto _ : state) {
